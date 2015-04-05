45ff19008ce603e7faab5aaa833a5283b3b1b9c0
Jeremy Rose <jeremy.rose09@gmail.com>
2015-04-04 22:38:09 -0400

    Created main.py -- used for packpanda to create installer for distribution purposes

---
+++ main.py
@@ -0,0 +1,9 @@
+# ----------------------------------------------------#
+#
+# main.py
+# Main file that is only used for distribution purposes 
+# by packpanda
+#
+# ----------------------------------------------------#
+from terminalZone import TerminalZone
+TerminalZone.run()
